863c6261bd71a2cc8b8f7a5ed48a8e8f988622df
aw168 <965868345@qq.com>
2025-04-26 18:53:49 +0800

    Delete usr/themes/Joe directory

--- usr/themes/Joe/module/qrcode.php
+++
@@ -1,8 +0,0 @@
-<?php
-$text = $_GET['text'];
-if ($text) {
-    require_once dirname(__DIR__) . '/system/library/QRcode.php';
-    joe\library\QRcode::png($_GET['text'], false, QR_ECLEVEL_L, 10, 1);
-} else {
-    echo '{"code":2,"msg":"参数不足!"}';
-}
