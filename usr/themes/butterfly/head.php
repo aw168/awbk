27e2ef28af179a4b6e06b7ef0a53fe0f89bd47de
aw168 <965868345@qq.com>
2025-05-11 21:39:02 +0800

    Delete usr/themes/butterfly directory

--- usr/themes/butterfly/head.php
+++
@@ -1,5 +0,0 @@
-<?php if (Helper::options()->commentsStyle !== 'off'): ?>
-<link rel="stylesheet" href="<?php $this->options->themeUrl('css/comments.css'); ?>">
-<link rel="stylesheet" href="<?php $this->options->themeUrl('css/comments-form.css'); ?>">
-<?php endif; ?>
-</head> 
--- usr/themes/butterfly/sitemap.xml.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * 站点地图XML入口文件
- */
-include_once 'sitemap.php';
-?> 
